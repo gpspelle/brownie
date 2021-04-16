d63abdffe565c96961b41636838cb475aef55717
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2021-04-16 23:59:29 +0200

    feat(login, mongo): after login the user data is updated on mongodb

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { OAuth2Client } = require('google-auth-library')
 const { MongoClient } = require('mongodb');
-
+const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 5000;
 const google_auth_client = new OAuth2Client(process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID)
@@ -10,11 +10,14 @@ const mongodb_user = process.env.REACT_APP_MONGODB_CLIENT_ID;
 const mongodb_secret_key = process.env.REACT_APP_MONGODB_SECRET_KEY;
 const mongodb_server = process.env.REACT_APP_MONGODB_SERVER;
 const mongodb_database = process.env.REACT_APP_MONGODB_DATABASE;
-const mongodb_database_collection = process.env.REACT_APP_MONGODB_DATABASE_COLLECTION
+const mongodb_database_collection = process.env.REACT_APP_MONGODB_DATABASE_COLLECTION;
 const mongodb_uri = `mongodb+srv://${mongodb_user}:${mongodb_secret_key}@${mongodb_server}/${mongodb_database}?retryWrites=true&w=majority`;
+const mongodb_client = new MongoClient(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json())
+app.use(session({ resave: true, secret: '123456', saveUninitialized: true}));
 
 app.post("/api/v1/auth/google", async (req, res) => {    
   const { token }  = req.body    
@@ -25,36 +28,19 @@ app.post("/api/v1/auth/google", async (req, res) => {
   
   const { name, email, picture } = ticket.getPayload();
 
-  MongoClient.connect(mongodb_uri, function(err, db) {
+  mongodb_client.connect(err => {
     if (err) throw err;
-    var dbo = db.db(mongodb_database);
+    var dbo = mongodb_client.db(mongodb_database);
 
     const query = { email: email };
-    const update = { $set: { name: name, email: email, picture: picture }};
+    const user = { name: name, email: email, picture: picture }
+    const update = { $set: user };
     const options = { upsert: true };
-    const user = dbo.collection(mongodb_database_collection).updateOne(query, update, options);
-    req.session.userId = user.id
+    dbo.collection(mongodb_database_collection).updateOne(query, update, options);
+    req.session.email = user.email
     res.json(user)
     res.status(201)
-    db.close();
-  }); 
-
-})
-
-app.use(async (req, res, next) => {
-
-  MongoClient.connect(mongodb_uri, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db(mongodb_database);
-
-    const query = { email: req.session.email };
-    const findResult = dbo.collection(mongodb_database_collection).find(query);
-    db.close();
-
-    req.user = findResult;
   });
-
-  next()
 })
 
 app.delete("/api/v1/auth/logout", async (req, res) => {
@@ -67,5 +53,10 @@ app.delete("/api/v1/auth/logout", async (req, res) => {
 
 app.get("/me", async (req, res) => {
   res.status(200)
-  res.json(req.user)
+  res.json(req.session.email)
+})
+
+app.get("/test", async (req, res) => {
+  res.status(200)
+  res.json({ message: "I am running and live"})
 })
